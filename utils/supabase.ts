0827e17a38ceb096dc36eb0c90f2afecf0b3878f
Srijit Valliappan <srijit125@gmail.com>
2026-02-19 17:40:04 +0530

    New supabase DB initiated

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -4,6 +4,6 @@ const supabaseUrl =
   process.env.REACT_APP_SUPABASE_URL || "http://192.168.0.184:54321";
 const supabaseKey =
   process.env.REACT_APP_SECRET_KEY ||
-  "eyJhbGciOiJFUzI1NiIsImtpZCI6ImI4MTI2OWYxLTIxZDgtNGYyZS1iNzE5LWMyMjQwYTg0MGQ5MCIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImV4cCI6MjA4NDI1Mzk4NH0.xGvGTqzY9X4xxz88IlIf8VwSg779Z8iaLHdz41NfkL9Gtp7wP4PpPbCGS2xVfmxleqvTa3lhyIxpLiOdocAw9A";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0";
 
 export const supabase = createClient(supabaseUrl!, supabaseKey!);
